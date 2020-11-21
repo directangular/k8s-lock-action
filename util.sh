53a2c3aa99317fb4f41b593831c85279f0be92d8
Mitchel Humpherys <mitch@directangular.com>
2020-11-20 16:53:09 -0800

    util.sh: Fix kube config data variable (needs INPUT_ prefix :facepalm:)

--- util.sh
+++ util.sh
@@ -1,6 +1,6 @@
 # Can exit. Invoke directly, not from a subshell.
 function init_k8s() {
-    base64 -d <<<"$KUBE_CONFIG_DATA" > /tmp/kubeconfig
+    base64 -d <<<"$INPUT_KUBE_CONFIG_DATA" > /tmp/kubeconfig
     export KUBECONFIG=/tmp/kubeconfig
     [[ -n "$INPUT_KUBE_CONTEXT" ]] && kubectl config use-context $INPUT_KUBE_CONTEXT
     kubectl version || { echo "Couldn't get a working kubectl"; exit 1; }
